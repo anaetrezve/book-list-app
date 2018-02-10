7e0413a3f11256fbd6a2ce47d1bee045951b8a09
Anaet Hossain <anaethoss@gmail.com>
2018-02-11 03:22:51 +0600

    First commit with adding books, clear fields, showing alerts:

---
+++ js/app.js
@@ -0,0 +1,82 @@
+// Book constructor
+function Book(title, author, isbn) {
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+}
+
+// UI constructor
+function UI() {}
+
+// Adding book list method
+UI.prototype.addBookToList = function (book) {
+  const list = document.getElementById('book-lists');
+
+  // Creating tr (table row) element
+  const tr = document.createElement('tr');
+  // Creating td or table column (table data) and append to tr
+  tr.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#">X</a></td>
+  `;
+  
+  list.appendChild(tr);
+};
+
+// method for clear fields
+UI.prototype.clearFields = function(title, author, isbn) {
+  title.value = '';
+  author.value = '';
+  isbn.value = '';
+
+  // Setting focus to title field
+  title.focus();
+};
+
+// Method for showing alert
+UI.prototype.showAlert = function(msg, cls) {
+  // creating div for show alert
+  const div = document.createElement('div');
+  // Adding class to div
+  div.className = `alert ${cls}`;
+  // adding message to div
+  div.textContent = `${msg}`;
+
+  // selecting form 
+  const form = document.querySelector('#book-form');
+  form.insertAdjacentElement('beforebegin', div);
+
+  setTimeout(function() {
+    document.querySelector('.alert').remove();
+  }, 3000);
+};
+
+// Event listeners
+document.getElementById('book-form').addEventListener('submit', function(e) {
+  e.preventDefault();
+  // Getting form values
+  const title = document.getElementById('title');
+  const author = document.getElementById('author');
+  const isbn = document.getElementById('isbn');
+  
+  // Creating book object
+  const book = new Book(title.value, author.value, isbn.value);
+  
+  // Creating UI object
+  const ui = new UI();
+
+  // validating fields
+  if (title.value === "" || author.value === "" || isbn.value === "") {
+    ui.showAlert('Please Fill empty fields!', 'error');
+  } else {
+    // Adding book to the list
+    ui.addBookToList(book);
+
+    // show success message
+    ui.showAlert('Book Added Successfully!', 'success');
+    // Clearing fields
+    ui.clearFields(title, author, isbn);
+  }
+});
